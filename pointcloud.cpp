49874a3a1f3a01cb809256c4d18862b979c4f548
Gaorui666 <114456024+Gaorui666@users.noreply.github.com>
2023-04-03 12:40:23 +0800

    Update pointcloud.cpp

--- pointcloud.cpp
+++ pointcloud.cpp
@@ -1,46 +1,38 @@
 #include"pointcloud.h"
 #include"show_randc.h"
-/*µãÔÆ¼ÆËã
-Input: Ğ£×¼¾ØÕóMc Mp obj Phase
+/*ç‚¹äº‘è®¡ç®—
+Input: æ ¡å‡†çŸ©é˜µMc Mp obj Phase
 Output: obj.X obj.Y obj.Z
 */
-Pattern pointcloud(Mat& Mc,Mat& Mp,Pattern& object,Mat& Phase)  //´«ÈëĞ£×¼¾ØÕó
+Pattern pointcloud(Mat& Mc,Mat& Mp,Pattern& object,Mat& Phase)  //ä¼ å…¥æ ¡å‡†çŸ©é˜µ
 {
-	//Í¼°¸xcºÍycµÄĞĞÁĞ¾ØÕó
+	//å›¾æ¡ˆxcå’Œycçš„è¡Œåˆ—çŸ©é˜µ
 	Mat orig_xc = Mat::ones(object.height, object.width, CV_64F);
 	Mat orig_yc = Mat::ones(object.height, object.width, CV_64F);
 	for (int ii = 1; ii < object.width; ii++)  //480
 	{
-		orig_xc.col(ii) = orig_xc.col(0)*(ii+1); //µÚiiĞĞÈ«²¿¼ÓÖ®Ç°Ò»ĞĞµÄÖµ,Ê¹µÃµÚ0ĞĞÎª1µÚ1ĞĞÈ«...
+		orig_xc.col(ii) = orig_xc.col(0)*(ii+1); //ç¬¬iiè¡Œå…¨éƒ¨åŠ ä¹‹å‰ä¸€è¡Œçš„å€¼,ä½¿å¾—ç¬¬0è¡Œä¸º1ç¬¬1è¡Œå…¨...
 		//string name = "orig_xc";
 		//Show_rowandcol(orig_xc, 1, 479, name);
 		
-	//...ÊÇ2£¬µÚ2ĞĞÈ«ÊÇ3
+	//...æ˜¯2ï¼Œç¬¬2è¡Œå…¨æ˜¯3
 	}
 	for (int jj = 1; jj < object.height; jj++)  //480
 	{
-		orig_yc.row(jj) = orig_yc.row(0)*(jj+1); //µÚiiÁĞÈ«²¿¼ÓÖ®Ç°Ò»ÁĞµÄÖµ,Ê¹µÃµÚ0ÁĞÎª1,µÚ1ÁĞÈ«...
-		//...ÊÇ2£¬µÚ2ĞĞÈ«ÊÇ3
+		orig_yc.row(jj) = orig_yc.row(0)*(jj+1); //ç¬¬iiåˆ—å…¨éƒ¨åŠ ä¹‹å‰ä¸€åˆ—çš„å€¼,ä½¿å¾—ç¬¬0åˆ—ä¸º1,ç¬¬1åˆ—å…¨...
+		//...æ˜¯2ï¼Œç¬¬2è¡Œå…¨æ˜¯3
 
 	}
-	//ÖÁ´ËĞÎ³ÉÁËÍ¼°¸xcºÍycµÄĞĞÁĞ¾ØÕó
+	//è‡³æ­¤å½¢æˆäº†å›¾æ¡ˆxcå’Œycçš„è¡Œåˆ—çŸ©é˜µ
 	cout << "Mc.at<double>(0, 0)" << Mc.at<double>(0, 0) << endl;
 	cout << "Mc.at<double>(2, 0)" << Mc.at<double>(2, 0) << endl;
 
 	Mat C11 = Mc.at<double>(0, 0) - Mc.at<double>(2, 0) * orig_xc;
 	Mat C12 = Mc.at<double>(0, 1) - Mc.at<double>(2, 1) * orig_xc;
 	Mat C13 = Mc.at<double>(0, 2) - Mc.at<double>(2, 2) * orig_xc;
-	//string name = "orig_xc";
-	//Show_rowandcol(orig_xc, 0, 100, name);
-	//string name_2 = "C13";
-	//Show_rowandcol(C13, 0, 100, name_2);
 	Mat C21 = Mc.at<double>(1, 0) - Mc.at<double>(2, 0) * orig_yc;
 	Mat C22 = Mc.at<double>(1, 1) - Mc.at<double>(2, 1) * orig_yc;
 	Mat C23 = Mc.at<double>(1, 2) - Mc.at<double>(2, 2) * orig_yc;
-	//string name_3 = "orig_yc";
-	//Show_rowandcol(orig_yc, 1, 100, name_3);   //ÏÔÊ¾100ÄÇÒ»ĞĞ
-	//string name_4 = "C23";
-	//Show_rowandcol(C23, 1, 100, name_4);
 	Mat C31 = Mp.at<double>(1, 0) - Mp.at<double>(2, 0) * Phase;
 	Mat C32 = Mp.at<double>(1, 1) - Mp.at<double>(2, 1) * Phase;
 	Mat C33 = Mp.at<double>(1, 2) - Mp.at<double>(2, 2) * Phase;
@@ -52,21 +44,9 @@ Pattern pointcloud(Mat& Mc,Mat& Mp,Pattern& object,Mat& Phase)  //´«ÈëĞ£×¼¾ØÕó
 	Mat detC = C11.mul(C22.mul(C33)) + C12.mul(C23.mul(C31)) + C13.mul(C21.mul(C32))
 		- C11.mul(C23.mul(C32)) - C12.mul(C21.mul(C33)) - C13.mul(C22.mul(C31));
 
-	//string name = "det_C";   //ÏÔÊ¾C¾ØÕóĞĞÁĞÊ½µÚ100ÁĞ
-	//Show_rowandcol(detC, 0, 0, name);
-	//Show_rowandcol(detC, 0, 99, name);
-	//string name_2 = "det_C";//ÏÔÊ¾C¾ØÕóĞĞÁĞÊ½µÚ100ĞĞ
-	//Show_rowandcol(detC, 1, 0, name_2);
-	//Show_rowandcol(detC, 1, 99, name_2);
 
 	Mat reverse_det = 1.0 / detC;
 
-	//string name_3 = "reverse_det";   //ÏÔÊ¾C¾ØÕóÇóµ¹ÊıĞĞÁĞÊ½µÚ100ÁĞ
-	//Show_rowandcol(reverse_det, 1, 0, name_3);
-	//Show_rowandcol(reverse_det, 0, 1, name_3);
-	//string name_4 = "reverse_det";//ÏÔÊ¾C¾ØÕóµ¹ÊıĞĞÁĞÊ½µÚ100ĞĞ
-	//Show_rowandcol(reverse_det, 1, 0, name_4);
-	//Show_rowandcol(reverse_det, 1, 99, name_4);
 
 	Mat A11 = C22.mul(C33) - C23.mul(C32);
 	Mat A12 = -C12.mul(C33) + C13.mul(C32);
@@ -77,15 +57,11 @@ Pattern pointcloud(Mat& Mc,Mat& Mp,Pattern& object,Mat& Phase)  //´«ÈëĞ£×¼¾ØÕó
 	Mat A31 = C21.mul(C32) - C22.mul(C31);
 	Mat A32 = -C11.mul(C32) + C21.mul(C31);
 	Mat A33 = C11.mul(C22) - C12.mul(C21);
-	//string name_4 = "test";   //ÏÔÊ¾C¾ØÕóÇóµ¹ÊıĞĞÁĞÊ½µÚ100ÁĞ
-	//Show_rowandcol(test, 1, 0, name_4);
+
 	object.X = reverse_det.mul( A11.mul(D11) + A12.mul(D21) + A13.mul(D31) );
-	//string name_5 = "object.X";   //ÏÔÊ¾C¾ØÕóÇóµ¹ÊıĞĞÁĞÊ½µÚ100ÁĞ
-	//Show_rowandcol(object.X, 1, 0, name_5);
 	object.Y = reverse_det.mul( A21.mul(D11) + A22.mul(D21) + A23.mul(D31) );
 	object.Z = reverse_det.mul( A31.mul(D11) + A32.mul(D21) + A33.mul(D31) );
-	//string name_6 = "object.Z";   //ÏÔÊ¾C¾ØÕóÇóµ¹ÊıĞĞÁĞÊ½µÚ100ÁĞ
-	//Show_rowandcol(object.Z, 1, 0, name_6);
+
 	return object;
 
 }
